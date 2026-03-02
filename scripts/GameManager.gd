680b31a486774911673de32cad9d6a39747cbbdc
Jaxon Fryling <jaxon@Jaxons-MacBook-Air.local>
2026-03-01 19:58:27 -0800

    fix bugs

--- scripts/GameManager.gd
+++ scripts/GameManager.gd
@@ -8,4 +8,8 @@ func change_gravity(new_dir: Vector2):
 		gravity_direction = new_dir
 		CameraShake.add_trauma(0.6)
 
-
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey and event.pressed and not event.echo:
+		if event.physical_keycode == KEY_R:
+			var current = get_tree().current_scene.scene_file_path
+			TransitionScreen.change_scene(current)
